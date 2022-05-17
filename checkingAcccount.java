268bdf9499008468ed1e59ec78f4d3eaf8ad4001
Atheer2244 <105737901+Atheer2244@users.noreply.github.com>
2022-05-17 20:32:29 +0300

    Update checkingAcccount.java

--- checkingAcccount.java
+++ checkingAcccount.java
@@ -7,22 +7,22 @@ class CheckingAccount extends BankSystem {
         super(name, accountType, amount);
 
     }
-
+// To calculat withdraw amount 
     public void withdraw(double amount) {
         double a = getAmount();
         amount = a - amount;
         setAmount(amount);
     }
-
+// retrieve the carde number in the object
     public String getCardNo() {
         return CardNo;
 
     }
-
+//set the carde number in the object
     public void setCardNo(String CardNo) {
         this.CardNo = CardNo;
     }
-
+//Get and display the user's bank information and return it as a string
     public String toString() {
 
         return "Name:" + getName() + "\nID:" + getID() + "\nAccount Type:" + getAccountType() + "\nAmount:" + (int) getAmount() + "\nSerial:" + CardNo;
